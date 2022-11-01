4e4a7e8a30b9fbdb018da31aeb2c411a6a4ab61b
Adakim4ik <Adakim4ik@gmail.com>
2022-11-01 18:45:58 +0200

    One save

---
+++ main.js
@@ -0,0 +1,42 @@
+let hello = 'Hello'
+let owu = 'owu'
+let com = 'com'
+let ua = 'ua'
+let numberOne = 1
+let numberTen = 10
+let numberbig = -999
+let numberOneTwoThree = 123
+let numberPi = 3.14
+let number = 2.7
+let nemberSixteen = 16
+let boolt = 5>2
+let boolf = 5>6
+console.log(hello)
+console.log(owu)
+console.log(com)
+console.log(ua)
+console.log(numberOne)
+console.log(numberTen)
+console.log(numberbig)
+console.log(numberOneTwoThree)
+console.log(numberPi)
+console.log(number)
+console.log(nemberSixteen)
+console.log(boolt)
+console.log(boolf)
+
+// hello','owu','com', 'ua', 1, 10, -999, 123, ' +
+// '3.14, 2.7, 16, true, false.
+
+let firstName = 'Akym'
+let middleName = 'Yuriyovych'
+let lastName = 'Adamenko'
+let person = (firstName + ' ' + middleName+ ' ' + lastName)
+console.log(person)
+
+let a = 100
+let b = '100'
+let c = 2>1
+console.log(typeof (a))
+console.log(typeof (b))
+console.log(typeof (c))
