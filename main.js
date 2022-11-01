aec6eddcce153a71689259db1433a8694be3c2d6
Adakim4ik <Adakim4ik@gmail.com>
2022-11-01 19:03:29 +0200

    AKYM

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ let lastName = 'Adamenko'
 let person = (firstName + ' ' + middleName+ ' ' + lastName)
 console.log(person)
 
-let a = 10
+let a = 100
 let b = '100'
 let c = 2>1
 console.log(typeof (a))
