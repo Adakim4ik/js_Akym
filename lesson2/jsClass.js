7c3df0d052fac2530f1088dc68968b5920ba046c
Adakim4ik <Adakim4ik@gmail.com>
2022-11-03 14:43:36 +0200

    HomWork

---
+++ lesson2/jsClass.js
@@ -0,0 +1,75 @@
+
+
+
+
+
+
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+let element = [1,'jff',true,false,23,'115','aaaa',3.14,2.7,999]
+console.log(element)
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+let book1 = {
+    title: 'book1',
+    pageCount: '300',
+    genre: 'drama',
+}
+let book2 = {
+    title: 'book2',
+    pageCount: '100',
+    genre: 'comedy',
+}
+let book3 = {
+    title: 'book3',
+    pageCount: '200',
+    genre: 'detective',
+}
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors.
+// Поле "автори"- масив. Кожен автор має поля name,age
+let book11 = {
+    title: 'book11',
+    pageCount: '400',
+    genre: 'detective',
+    authors: [name = 'Petya', age = 37 ]
+}
+
+let book22 = {
+    title: 'book22',
+    pageCount: '300',
+    genre: 'comedy',
+    authors: [name = 'Artem', age = 23 ]
+}
+
+let book33 = {
+    title: 'book33',
+    pageCount: '100',
+    genre: 'drama',
+    authors: [name = 'Andriy', age = 40 ]
+}
+
+ //- Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password.' +
+ //' Вивести в консоль пароль кожного користувача
+let database = [
+    {name: 'Petya', username: 'pet', password:12345},
+    {name:'Qas',username:'asdas',password: 54321},
+    {name:'Asd',username:'hfbg',password: 21543},
+    {name:'Zcxz',username:'fere',password: 23145},
+    {name:'Efsd',username:'wds',password: 35124},
+    {name:'Wsdf',username:'uht',password: 24152},
+    {name:'Dhf',username:'qwwd',password: 22233},
+    {name:'Cfe',username:'sdfd',password: 11324},
+    {name:'Hfss',username:'1w2w',password: 12452},
+    {name:'Aads',username:'ujyhy',password: 14234}
+]
+
+console.log(database [0].password)
+console.log(database [1].password)
+console.log(database [2].password)
+console.log(database [3].password)
+console.log(database [4].password)
+console.log(database [5].password)
+console.log(database [6].password)
+console.log(database [7].password)
+console.log(database [8].password)
+console.log(database [9].password)
